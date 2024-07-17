6479ced47d8982f8936b249ec5b95fa78b6f5fbb
yubin <cyoobin@naver.com>
2024-07-17 16:11:51 +0900

    Design: 모집글 작성 플로팅 버튼 UI 구현

--- src/app/(nav)/home/page.tsx
+++ src/app/(nav)/home/page.tsx
@@ -3,16 +3,21 @@ import LocationSettingButton from '@/containers/home/LocationSettingButton';
 import ArticleList from '@/containers/home/ArticleList';
 import Ranking from '@/containers/home/Ranking';
 import Filter from '@/containers/home/Filter';
+import WriteButton from '@/containers/home/WriteButton';
 
 const page = () => {
   return (
-    <div>
+    <>
       <Banner />
       <Ranking />
       <LocationSettingButton />
       <Filter />
       <ArticleList />
-    </div>
+      <div className="text-center translate-y-5 text-gray-600 text-sm">
+        모든 글을 확인하셨습니다
+      </div>
+      <WriteButton />
+    </>
   );
 };
 
