6794844d16a8406a3997b4a4e9b0adfb29aa0435
yubin <cyoobin@naver.com>
2024-07-22 00:50:07 +0900

    Fix: 빌드 시 undefined 오류 해결

--- src/app/(nav)/home/page.tsx
+++ src/app/(nav)/home/page.tsx
@@ -3,6 +3,9 @@ import ArticleList from '@/containers/home/ArticleList';
 import Ranking from '@/containers/home/Ranking';
 import WriteButton from '@/containers/home/WriteButton';
 
+export const dynamic = 'force-dynamic';
+export const fetchCache = 'force-no-store';
+
 const page = () => {
   return (
     <>
--- src/app/(without-nav)/setting/location/page.tsx
+++ src/app/(without-nav)/setting/location/page.tsx
@@ -1,6 +1,9 @@
 import LocationRadiusSetting from '@/containers/location/LocationRadiusSetting';
 import Map from '@/containers/location/Map';
 
+export const dynamic = 'force-dynamic';
+export const fetchCache = 'force-no-store';
+
 const page = () => {
   return (
     <div className="flex flex-col min-h-full">
