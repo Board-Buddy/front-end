f7e48516e1e19c9fc2f4591607b3dc4c2f5fc2eb
yubin <cyoobin@naver.com>
2024-07-22 17:27:56 +0900

    Fix: url 오타 수정

--- src/app/(without-nav)/(auth)/login/page.tsx
+++ src/app/(without-nav)/(auth)/login/page.tsx
@@ -25,7 +25,7 @@ const page = () => {
         <Separator className="bg-gray-600 flex-1" />
       </div>
       <div className="flex justify-center gap-16 mt-6">
-        <Link href="https://boardbuddyapp.com/api/oauth/authorization/naver">
+        <Link href="https://boardbuddyapp.com/api/oauth2/authorization/naver">
           <Image
             src="/images/icon/naver_icon.png"
             alt="네이버 로그인"
@@ -33,7 +33,7 @@ const page = () => {
             height={35}
           />
         </Link>
-        <Link href="https://boardbuddyapp.com/api/oauth/authorization/kakao">
+        <Link href="https://boardbuddyapp.com/api/oauth2/authorization/kakao">
           <Image
             src="/images/icon/kakao_icon.png"
             alt="카카오 로그인"
@@ -41,7 +41,7 @@ const page = () => {
             height={35}
           />
         </Link>
-        <Link href="https://boardbuddyapp.com/api/oauth/authorization/google">
+        <Link href="https://boardbuddyapp.com/api/oauth2/authorization/google">
           <Image
             src="/images/icon/google_icon.png"
             alt="구글 로그인"
