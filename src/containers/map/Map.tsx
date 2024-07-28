e3e5ae869cb2565e2e379f03d805054411e63c25
yubin <cyoobin@naver.com>
2024-07-28 16:12:41 +0900

    Refactor: 불필요한 코드 삭제

--- src/containers/map/Map.tsx
+++ src/containers/map/Map.tsx
@@ -8,7 +8,6 @@ import useKakaoMap from '@/hooks/custom/useKakaoMap';
 import useCafesMarkers from '@/hooks/custom/useCafesMarkers';
 import usePanToCafe from '@/hooks/custom/usePanToCafe';
 import ReloadButton from './ReloadButton';
-import MapInfo from './CafeInfo';
 
 declare global {
   interface Window {
