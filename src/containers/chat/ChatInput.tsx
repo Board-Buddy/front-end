c9ba8f74047df6b188d8e12bf74f9993e81662c2
yubin <cyoobin@naver.com>
2024-07-30 13:30:42 +0900

    Design: 채팅이 입력되었는지에 따라 버튼 색상 변경

--- src/containers/chat/ChatInput.tsx
+++ src/containers/chat/ChatInput.tsx
@@ -1,13 +1,29 @@
+'use client';
+
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
+import { cn } from '@/utils/tailwind';
 import { ArrowUp } from 'lucide-react';
+import { useState } from 'react';
 
 const ChatInput = () => {
+  const [value, setValue] = useState('');
+
   return (
     <div className="flex items-center gap-2 fixed bottom-0 p-4 max-w-md w-full bg-white">
-      <Input className="bg-gray-100" />
-      <Button className="rounded-full p-2 size-9">
-        <ArrowUp className="text-white" />
+      <Input
+        className="bg-gray-100"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <Button
+        className={cn(
+          'rounded-full p-2 size-10',
+          value ? 'bg-primary' : 'bg-gray-200',
+        )}
+        disabled={value === ''}
+      >
+        <ArrowUp className={cn(value ? 'text-white' : 'text-gray-700')} />
       </Button>
     </div>
   );
