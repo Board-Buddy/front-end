176a3b0d89aa16086d749fefd93e623731e3dd19
mhjoon99 <hyejoon8@gmail.com>
2024-07-31 15:20:35 +0900

    Design:마이페이지 뱃지 목록 UI 구현

--- src/containers/profile/ReviewList.tsx
+++ src/containers/profile/ReviewList.tsx
@@ -1,5 +1,13 @@
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+
 const ReviewList = () => {
-  return <div className="text-lg flex gap-2 font-bold">받은 후기</div>;
+  return (
+    <div className="border-b-[1px] border-gray-200 p-4">
+      <div className="flex justify-between items-center text-lg font-bold mb-4">
+        <div className="flex gap-2">받은 후기</div>
+      </div>
+    </div>
+  );
 };
 
 export default ReviewList;
