c7ce1ad3b45d0a93c5aeec88dd8730a776324e95
yubin <cyoobin@naver.com>
2024-07-31 18:35:49 +0900

    Refactor: 랭킹 관련 API 에러 핸들링 로직 리팩토링

--- src/services/ranking.ts
+++ src/services/ranking.ts
@@ -1,5 +1,12 @@
 import api from '@/services';
+import { handleApiError } from '@/utils/handleApiError';
 
 /** 랭킹 조회 API */
-export const getRankings = () =>
-  api.get(`/api/rankings`).then((response) => response.data.data.rankings);
+export const getRankings = async () => {
+  try {
+    const response = await api.get(`/api/rankings`);
+    return response.data.data.rankings;
+  } catch (error: unknown) {
+    handleApiError(error);
+  }
+};
