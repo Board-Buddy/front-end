fa96757fa3d021ea7f74a3932207797e8a0b628a
yubin <cyoobin@naver.com>
2024-07-31 20:30:22 +0900

    Refactor: 채팅 관련 API 에러 핸들링 로직 리팩토링

--- src/services/chat.ts
+++ src/services/chat.ts
@@ -1,22 +1,37 @@
+import { handleApiError } from '@/utils/handleApiError';
 import api from '.';
 
 /** 채팅 기존 메시지 조회 API */
-export const getExistingMessages = (chatRoomId: number | string) =>
-  api
-    .get(`/api/chat/rooms/${chatRoomId}/messages`)
-    .then((response) => response.data.data.chatMessages);
+export const getExistingMessages = async (chatRoomId: number | string) => {
+  try {
+    const response = await api.get(`/api/chat/rooms/${chatRoomId}/messages`);
+    return response.data.data.chatMessages;
+  } catch (error: unknown) {
+    handleApiError(error);
+  }
+};
 
 /** 채팅 목록 조회 API */
-export const getChatList = () =>
-  api
-    .get('/api/chat/rooms')
-    .then((response) => response.data.data.chatRoomDetailsList);
+export const getChatList = async () => {
+  try {
+    const response = await api.get('/api/chat/rooms');
+    return response.data.data.chatRoomDetailsList;
+  } catch (error: unknown) {
+    handleApiError(error);
+  }
+};
 
 /** 채팉방과 연결된 모집글 정보 축약 조회 */
-export const getArticleSimpleInfo = (
+export const getArticleSimpleInfo = async (
   chatRoomId: number | string,
   articleId: number | string,
-) =>
-  api
-    .get(`/api/chat/rooms/${chatRoomId}/gather-articles/${articleId}`)
-    .then((response) => response.data.data.gatherArticleSimpleInfo);
+) => {
+  try {
+    const response = await api.get(
+      `/api/chat/rooms/${chatRoomId}/gather-articles/${articleId}`,
+    );
+    return response.data.data.gatherArticleSimpleInfo;
+  } catch (error: unknown) {
+    handleApiError(error);
+  }
+};
