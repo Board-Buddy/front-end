e94177f899e7bf755bc227a0dfbb2e3801d1ed63
yubin <cyoobin@naver.com>
2024-07-31 23:03:21 +0900

    Fix: handleApiError return->throw로 수정

--- src/utils/handleApiError.ts
+++ src/utils/handleApiError.ts
@@ -3,14 +3,14 @@ import { CustomError } from '@/types/api';
 
 export const handleApiError = (error: unknown): CustomError => {
   if (axios.isAxiosError(error) && error.response) {
-    return {
+    throw {
       name: 'Axios Error',
       status: error.response.status,
       message: error.response.data.message || 'Something went wrong',
       data: error.response.data,
     };
   }
-  return {
+  throw {
     name: 'Network Error',
     status: 'error',
     message: 'Network Error',
