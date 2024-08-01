202c145d435b1669a901b1851b0055bf1031c7e3
yubin <cyoobin@naver.com>
2024-08-01 18:33:50 +0900

    Refactor: article API 에러 핸들링 로직 리팩토링

--- src/services/article.ts
+++ src/services/article.ts
@@ -1,60 +1,28 @@
 import api from '@/services';
 import { ArticleRequest, NewArticle } from '@/types/article';
-import { handleApiError } from '@/utils/handleApiError';
 
 /** 모집글 리스트 조회 API */
-export const getArticles = async ({
-  pageParam,
-  status,
-  sort,
-}: ArticleRequest) => {
-  try {
-    const response = await api.get('/api/gather-articles', {
+export const getArticles = ({ pageParam, status, sort }: ArticleRequest) =>
+  api
+    .get('/api/gather-articles', {
       params: { page: pageParam, status, sort },
-    });
-    return response.data.data;
-  } catch (error: unknown) {
-    handleApiError(error);
-  }
-};
+    })
+    .then((response) => response.data.data);
 
 /** 모집글 상세 조회 API */
-export const getArticle = async ({
-  gatherArticleId,
-}: {
-  gatherArticleId: number;
-}) => {
-  try {
-    const response = await api.get(`/api/gather-articles/${gatherArticleId}`);
-    return response.data.data.post;
-  } catch (error: unknown) {
-    handleApiError(error);
-  }
-};
+export const getArticle = ({ gatherArticleId }: { gatherArticleId: number }) =>
+  api
+    .get(`/api/gather-articles/${gatherArticleId}`)
+    .then((response) => response.data.data.post);
 
 /** 모집글 작성 API */
-export const addArticle = async (data: NewArticle) => {
-  try {
-    await api.post('/api/gather-articles', data);
-  } catch (error: unknown) {
-    handleApiError(error);
-  }
-};
+export const addArticle = (data: NewArticle) =>
+  api.post('/api/gather-articles', data);
 
 /** 모집글 수정 API */
-export const editArticle = async (data: NewArticle, articleId: number) => {
-  try {
-    await api.put(`/api/gather-articles/${articleId}`, data);
-  } catch (error: unknown) {
-    handleApiError(error);
-  }
-};
+export const editArticle = (data: NewArticle, articleId: number) =>
+  api.put(`/api/gather-articles/${articleId}`, data);
 
 /** 모집글 삭제 API */
-export const deleteArticle = async (articleId: number) => {
-  try {
-    await api.delete(`/api/gather-articles/${articleId}`);
-  } catch (error: unknown) {
-    handleApiError(error);
-  }
-};
+export const deleteArticle = (articleId: number) =>
+  api.delete(`/api/gather-articles/${articleId}`);
