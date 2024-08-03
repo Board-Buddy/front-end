59a6be9bc21e28ea074387ffd58511e7d32e5025
yubin <cyoobin@naver.com>
2024-08-03 18:35:28 +0900

    Feat: 알림 수신 시 토스트 메시지 띄우기 추가

--- src/components/NotificationProvider.tsx
+++ src/components/NotificationProvider.tsx
@@ -2,13 +2,29 @@
 
 import { API_BASE_URL } from '@/constants/env';
 import { useEffect } from 'react';
+import toast from 'react-hot-toast';
 
 const NotificationProvider = () => {
+  const notify = (message: string) => {
+    toast(message, {
+      icon: '🔔',
+      style: {
+        fontSize: '14px',
+        fontStyle: '600',
+      },
+    });
+  };
+
   useEffect(() => {
     const eventSource = new EventSource(`${API_BASE_URL}/api/subscribe`);
 
     eventSource.onmessage = (event) => {
       console.log('Received notification:', event.data);
+
+      const newNotification = JSON.parse(event.data);
+      const newNotificationContent = newNotification.data.notification.content;
+
+      notify(newNotificationContent);
     };
   });
   return <></>;
