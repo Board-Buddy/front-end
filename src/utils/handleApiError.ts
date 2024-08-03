3d1643322ed5aea5a61d63208f1e2f8ada34f78a
yubin <cyoobin@naver.com>
2024-08-03 18:35:39 +0900

    Design: 에러 토스트 메시지 디자인 수정

--- src/utils/handleApiError.ts
+++ src/utils/handleApiError.ts
@@ -2,6 +2,16 @@ import { CustomError } from '@/types/api';
 import axios from 'axios';
 import toast from 'react-hot-toast';
 
+const notify = (id: string, message: string) => {
+  toast.error(message, {
+    id: id,
+    style: {
+      fontSize: '14px',
+      fontWeight: '600',
+    },
+  });
+};
+
 const handleApiError = (error: unknown) => {
   if (axios.isAxiosError(error)) {
     // Axios 에러인 경우
@@ -10,29 +20,17 @@ const handleApiError = (error: unknown) => {
       const errorResponse = error.response?.data as CustomError;
 
       // 의도한 에러 처리
-      toast.error(errorResponse.message, {
-        id: 'axios error',
-        style: {
-          fontSize: '14px',
-        },
-      });
+      notify('axios error', errorResponse.message);
     } else {
       // error.response가 존재하지 않는 경우에는 서버 연결이 원활하지 않은 것으로 간주
-      toast.error('서버 연결이 원활하지 않습니다.', {
-        id: 'server error',
-        style: {
-          fontSize: '14px',
-        },
-      });
+      notify('server error', '서버 연결이 원활하지 않습니다.');
     }
   } else {
     // Axios 에러가 아닌 경우에는 네트워크 연결 오류나 기타 오류로 간주
-    toast.error('네트워크 연결 오류 또는 기타 오류가 발생했습니다.', {
-      id: 'network error',
-      style: {
-        fontSize: '14px',
-      },
-    });
+    notify(
+      'network error',
+      '네트워크 연결 오류 또는 기타 오류가 발생했습니다.',
+    );
   }
 };
 
