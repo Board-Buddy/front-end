735b6978b8c71d2df6c4c83d14a50fdad97fe028
yubin <cyoobin@naver.com>
2024-08-03 16:41:25 +0900

    Design: NotificationItem 알림 수신 일시 css 수정

--- src/app/(without-nav)/notifications/page.tsx
+++ src/app/(without-nav)/notifications/page.tsx
@@ -3,12 +3,12 @@ import NotificationItem from '@/containers/notifications/NotificationItem';
 const page = () => {
   const notifications = [
     {
-      message: "lee 님이 '퇴근 후 보드게임...'에 참가 신청을 했습니다.",
-      createdAt: '2024-08-02 14:54',
+      message: "lee 님이 '퇴근 후 보드게임...'의 참가 신청을 취소했습니다.",
+      createdAt: '2024-08-02 16:54',
     },
     {
       message: "lee 님이 '퇴근 후 보드게임...'의 참가 신청을 취소했습니다.",
-      createdAt: '2024-08-02 14:54',
+      createdAt: '2023-12-02 14:54',
     },
     {
       message: "lee 님이 '퇴근 후 보드게임...'에 참가 신청을 했습니다.",
