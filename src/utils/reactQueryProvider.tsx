29b409dd33062a7b8a85ea2e02648f7f2acd53e3
yubin <cyoobin@naver.com>
2024-08-06 01:40:12 +0900

    Fix: reactQueryProvider 이전대로 복구

--- src/utils/reactQueryProvider.tsx
+++ src/utils/reactQueryProvider.tsx
@@ -1,39 +1,33 @@
 'use client';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import {
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import React, { useState } from 'react';
 import handleApiError from './handleApiError';
-
-const makeQueryClient = () => {
-  return new QueryClient({
-    defaultOptions: {
-      queries: {
-        staleTime: Infinity,
-        gcTime: Infinity,
-      },
-      mutations: {
-        onError: handleApiError,
-      },
-    },
-  });
-};
-
-let browserQueryClient: QueryClient;
-
-const getQueryClient = () => {
-  if (typeof window === 'undefined') {
-    // Server일 경우 매번 새로운 queryClient를 만든다.
-    return makeQueryClient();
-  }
-  // Browser일 경우, queryClient가 존재하지 않는 경우에만 새로운 queryClient를 만든다.
-  if (!browserQueryClient) browserQueryClient = makeQueryClient();
-  return browserQueryClient;
-};
 
 export default function ReactQueryProviders({
   children,
 }: React.PropsWithChildren) {
-  const queryClient = getQueryClient();
+  const [queryClient] = useState(
+    new QueryClient({
+      defaultOptions: {
+        queries: {
+          staleTime: Infinity,
+          gcTime: Infinity,
+        },
+        mutations: {
+          onError: handleApiError,
+        },
+      },
+      queryCache: new QueryCache({
+        // onSuccess:
+      }),
+    }),
+  );
 
   return (
     <QueryClientProvider client={queryClient}>
