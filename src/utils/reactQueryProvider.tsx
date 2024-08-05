2f8cc6d96bf24d0217987a3ae35deb61dcfdf822
yubin <cyoobin@naver.com>
2024-08-06 00:45:23 +0900

    Feat: Hydration API를 사용하기 위한 초기 설정 서버용 queryClient와 브라우저용 queryClient 생성 로직 분기 처리

--- src/utils/reactQueryProvider.tsx
+++ src/utils/reactQueryProvider.tsx
@@ -1,33 +1,40 @@
 'use client';
 
-import {
-  QueryCache,
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import React, { useState } from 'react';
 import handleApiError from './handleApiError';
+
+const makeQueryClient = () => {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: Infinity,
+        gcTime: Infinity,
+      },
+      mutations: {
+        onError: handleApiError,
+      },
+    },
+  });
+};
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
+const getQueryClient = () => {
+  if (typeof window === 'undefined') {
+    // Server일 경우 매번 새로운 queryClient를 만든다.
+    return makeQueryClient();
+  } else {
+    // Browser일 경우, queryClient가 존재하지 않는 경우에만 새로운 queryClient를 만든다.
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
+  }
+};
 
 export default function ReactQueryProviders({
   children,
 }: React.PropsWithChildren) {
-  const [queryClient] = useState(
-    new QueryClient({
-      defaultOptions: {
-        queries: {
-          staleTime: Infinity,
-          gcTime: Infinity,
-        },
-        mutations: {
-          onError: handleApiError,
-        },
-      },
-      queryCache: new QueryCache({
-        // onSuccess:
-      }),
-    }),
-  );
+  const queryClient = getQueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
