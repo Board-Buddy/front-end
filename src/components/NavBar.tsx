0710b8f3844c49a572e86fa6ba3be07445501d4c
yubin <cyoobin@naver.com>
2024-08-08 00:22:48 +0900

    Fix: Navbar 기본값 수정, 항목 이름 수정

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,20 +3,25 @@
 import { cn } from '@/utils/tailwind';
 import Link from 'next/link';
 import { useState } from 'react';
+import { usePathname } from 'next/navigation';
 import HomeIcon from './svg/HomeIcon';
 import MapIcon from './svg/MapIcon';
 import ChatIcon from './svg/ChatIcon';
 import MyIcon from './svg/MyIcon';
 
 const NavBar = () => {
+  const pathname = usePathname();
+
   const navBarList = [
     { icon: HomeIcon, to: '/home', title: '홈' },
-    { icon: ChatIcon, to: '/chat', title: '메시지' },
-    { icon: MapIcon, to: '/map', title: '카페 위치' },
+    { icon: ChatIcon, to: '/chat', title: '채팅' },
+    { icon: MapIcon, to: '/map', title: '지도' },
     { icon: MyIcon, to: '/my', title: '마이페이지' },
   ];
 
-  const [focusedItem, setFocusedItem] = useState(navBarList[0].title);
+  const [focusedItem, setFocusedItem] = useState(
+    navBarList.filter((nav) => pathname.includes(nav.to))[0].title,
+  );
 
   return (
     <ul className="flex items-center h-16 px-6 py-4 border-t border-gray-200">
