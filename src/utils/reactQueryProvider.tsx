e25014b30a86f46497724d5dff61dd6ab0ae522b
yubin <cyoobin@naver.com>
2024-08-08 14:44:59 +0900

    Feat: 서버/클라이언트 용 queryClient 생성을 구분하기 위해 reactQueryProvider 수정

--- src/utils/reactQueryProvider.tsx
+++ src/utils/reactQueryProvider.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import {
+  isServer,
   QueryCache,
   QueryClient,
   QueryClientProvider,
@@ -9,25 +10,42 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React, { useState } from 'react';
 import handleApiError from './handleApiError';
 
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+      },
+      mutations: {
+        onError: handleApiError,
+      },
+    },
+  });
+}
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
+function getQueryClient() {
+  if (isServer) {
+    // Server일 경우
+    // 매번 새로운 queryClient를 만든다.
+    return makeQueryClient();
+  } else {
+    // Browser일 경우
+    // queryClient가 존재하지 않을 경우에만 새로운 queryClient를 만든다.
+    // React가 새 Client를 만들게 하기 위해 중요하다.
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
+  }
+}
+
 export default function ReactQueryProviders({
   children,
 }: React.PropsWithChildren) {
-  const [queryClient] = useState(
-    new QueryClient({
-      defaultOptions: {
-        queries: {
-          staleTime: Infinity,
-          gcTime: Infinity,
-        },
-        mutations: {
-          onError: handleApiError,
-        },
-      },
-      queryCache: new QueryCache({
-        // onSuccess:
-      }),
-    }),
-  );
+  // queryClient를 useState를 사용하여 초기화 하면 안된다.
+  // suspense boundary가 없을 경우 React의 렌더링이 중단될 수도 있고
+  // queryClient 자체를 폐기할 수 도 있다.
+  const queryClient = getQueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
