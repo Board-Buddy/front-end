03af826ea2358f23782a09f1b1000eabb6142fad
yubin <cyoobin@naver.com>
2024-08-08 15:21:04 +0900

    Feat: 삭제 확인 alert/confirm CustomAlert 컴포넌트로 모두 변경

--- src/containers/article/ArticleContent.tsx
+++ src/containers/article/ArticleContent.tsx
@@ -19,6 +19,8 @@ import { useWriteFormContext } from '@/context/WriteFormContext';
 import { useRouter } from 'next/navigation';
 import { ARTICLE_STATUS } from '@/constants/article';
 import { useDeleteArticle } from '@/hooks/useArticle';
+import CustomAlert from '@/components/CustomAlert';
+import { useState } from 'react';
 import Map from './Map';
 
 interface Props extends Omit<Article, 'author'> {
@@ -46,6 +48,8 @@ const ArticleContent = ({
   const { setFormState } = useWriteFormContext();
   const router = useRouter();
 
+  const [open, setOpen] = useState(false);
+
   const mutation = useDeleteArticle(id);
 
   const handleEditButtonClick = () => {
@@ -72,89 +76,94 @@ const ArticleContent = ({
     router.push(`/article/${id}/edit`);
   };
 
-  const handleRemoveButtonClick = () => {
-    if (window.confirm('정말 삭제하시겠습니까?')) {
-      mutation.mutate();
-    }
-  };
-
   return (
-    <div className="p-4">
-      <div className="text-lg flex gap-2 font-bold ">
-        <span
-          className={cn(status === 'open' ? 'text-primary' : 'text-gray-500')}
-        >
-          {ARTICLE_STATUS[status]}
-        </span>
-        <span className="text-gray-800 ">{title}</span>
-        <DropdownMenu>
-          <DropdownMenuTrigger
-            className={cn(
-              isAuthor ? 'visible' : 'hidden',
-              'ml-auto cursor-pointer',
-            )}
+    <>
+      <div className="p-4">
+        <div className="text-lg flex gap-2 font-bold ">
+          <span
+            className={cn(status === 'open' ? 'text-primary' : 'text-gray-500')}
           >
-            <EllipsisVerticalIcon />
-            <DropdownMenuContent className="bg-white mt-1 -ml-8 w-16">
-              <DropdownMenuItem
-                className="hover:bg-slate-50 transition-all"
-                onClick={handleEditButtonClick}
-              >
-                수정
-              </DropdownMenuItem>
-              <DropdownMenuItem
-                className="hover:bg-slate-50 transition-all"
-                onClick={handleRemoveButtonClick}
-              >
-                삭제
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenuTrigger>
-        </DropdownMenu>
-      </div>
-      <div className="text-md mt-2 text-gray-800 py-4">{description}</div>
-      <div className="text-sm">
-        <div className="mr-auto text-gray-500 py-3">
-          {formatRelativeTime(createdAt)}
+            {ARTICLE_STATUS[status]}
+          </span>
+          <span className="text-gray-800 ">{title}</span>
+          <DropdownMenu>
+            <DropdownMenuTrigger
+              className={cn(
+                isAuthor ? 'visible' : 'hidden',
+                'ml-auto cursor-pointer',
+              )}
+            >
+              <EllipsisVerticalIcon />
+              <DropdownMenuContent className="bg-white mt-1 -ml-8 w-16">
+                <DropdownMenuItem
+                  className="hover:bg-slate-50 transition-all"
+                  onClick={handleEditButtonClick}
+                >
+                  수정
+                </DropdownMenuItem>
+                <DropdownMenuItem
+                  className="hover:bg-slate-50 transition-all"
+                  onClick={() => setOpen(true)}
+                >
+                  삭제
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenuTrigger>
+          </DropdownMenu>
         </div>
-        <div>
-          <div className="text-gray-600 flex items-center">
+        <div className="text-md mt-2 text-gray-800 py-4">{description}</div>
+        <div className="text-sm">
+          <div className="mr-auto text-gray-500 py-3">
+            {formatRelativeTime(createdAt)}
+          </div>
+          <div>
+            <div className="text-gray-600 flex items-center">
+              <Image
+                src="/images/icon/participants_icon.png"
+                alt="participants"
+                width={12}
+                height={12}
+                className="mr-1"
+              />
+              {currentParticipants}/{maxParticipants}명 참여
+            </div>
+            <div className="text-gray-700 flex items-center">
+              <Image
+                src="/images/icon/clock_icon.png"
+                alt="clock"
+                width={12}
+                height={12}
+                className="mr-1"
+              />
+              {formatMeetingTime(startDateTime, endDateTime)}
+            </div>
+          </div>
+        </div>
+        <div className="mt-10">
+          <span className="text-gray-700 text-lg font-bold">위치</span>
+          <div className="text-secondary flex items-center text-sm mt-2 mb-2">
             <Image
-              src="/images/icon/participants_icon.png"
-              alt="participants"
+              src="/images/icon/map_icon.png"
+              alt="map"
               width={12}
               height={12}
               className="mr-1"
             />
-            {currentParticipants}/{maxParticipants}명 참여
+            {meetingLocation}
           </div>
-          <div className="text-gray-700 flex items-center">
-            <Image
-              src="/images/icon/clock_icon.png"
-              alt="clock"
-              width={12}
-              height={12}
-              className="mr-1"
-            />
-            {formatMeetingTime(startDateTime, endDateTime)}
-          </div>
+          <Map lat={y!} lng={x!} />
         </div>
       </div>
-      <div className="mt-10">
-        <span className="text-gray-700 text-lg font-bold">위치</span>
-        <div className="text-secondary flex items-center text-sm mt-2 mb-2">
-          <Image
-            src="/images/icon/map_icon.png"
-            alt="map"
-            width={12}
-            height={12}
-            className="mr-1"
-          />
-          {meetingLocation}
-        </div>
-        <Map lat={y!} lng={x!} />
-      </div>
-    </div>
+      <CustomAlert
+        open={open}
+        setOpen={setOpen}
+        title="정말 삭제하시겠습니까?"
+        description="삭제하시면 복구할 수 없습니다."
+        cancelText="아니요"
+        confirmText="네"
+        onConfirm={() => mutation.mutate()}
+      />
+    </>
   );
 };
 
--- src/containers/article/CommentList.tsx
+++ src/containers/article/CommentList.tsx
@@ -15,12 +15,17 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
+import CustomAlert from '@/components/CustomAlert';
 import CommentInput from './CommentInput';
 
 const CommentList = ({ articleId }: { articleId: number }) => {
   const cache = useQueryClient();
   const userInfo = cache.getQueryData(['userInfo']) as UserInfo;
   const { nickname } = userInfo;
+
+  const [openCommentDeleteAlert, setOpenCommentDeleteAlert] = useState(false);
+  const [deleteCommentId, setDeleteCommentId] = useState('');
+  const [openReplyDeleteAlert, setOpenReplyDeleteAlert] = useState(false);
 
   const [parentComment, setParentComment] = useState<{
     parentId: string;
@@ -62,157 +67,174 @@ const CommentList = ({ articleId }: { articleId: number }) => {
     setEditingComment({ id, content });
   };
 
-  const handleRemoveButtonClick = (id: string) => {
-    if (window.confirm('정말 삭제하시겠습니까?')) {
-      deleteCommentMutation.mutate(id);
-    }
-  };
-
   return (
-    <div className="p-4">
-      <span className="text-gray-700 text-lg font-bold">댓글</span>
-      {commentList.map((comment) => (
-        <div key={comment.id}>
-          <div
-            key={comment.id}
-            className="p-4 my-2 last-of-type:border-none border-b-[1px] border-gray-200"
-          >
-            <div className="flex items-center gap-2 mb-2">
-              <CustomAvatar
-                src={comment.author.profileImageS3SavedURL || ''}
-                rank={comment.author.rank}
-                nickname={comment.author.nickname}
-                avatarSize="xs"
-              />
-              <p className="text-sm">{comment.author.nickname}</p>
-              <div className="ml-auto flex gap-2">
-                <Button className="bg-transparent p-0">
-                  <MessageSquare
-                    className="text-gray-400 size-4"
-                    onClick={() =>
-                      handleReplyButtonClick(
-                        comment.id.toString(),
-                        comment.author.nickname,
-                      )
-                    }
-                  />
-                </Button>
-                <DropdownMenu>
-                  <DropdownMenuTrigger
-                    className={cn(
-                      nickname === comment.author.nickname
-                        ? 'visible'
-                        : 'hidden',
-                      'ml-auto cursor-pointer',
-                    )}
-                  >
-                    <Ellipsis className="text-gray-400 size-4" />
-                    <DropdownMenuContent className="bg-white -mt-2 -ml-8 w-16">
-                      <DropdownMenuItem
-                        className="hover:bg-slate-50 transition-all"
-                        onClick={() =>
-                          handleEditButtonClick(
-                            comment.id.toString(),
-                            comment.content,
-                          )
-                        }
-                      >
-                        수정
-                      </DropdownMenuItem>
-                      <DropdownMenuItem
-                        className="hover:bg-slate-50 transition-all"
-                        onClick={() =>
-                          handleRemoveButtonClick(comment.id.toString())
-                        }
-                      >
-                        삭제
-                      </DropdownMenuItem>
-                    </DropdownMenuContent>
-                  </DropdownMenuTrigger>
-                </DropdownMenu>
-              </div>
-            </div>
-            <p className="text-sm">{comment.content}</p>
-            <p className="text-sm mt-2 text-gray-500">
-              {commentTime(comment.createdAt)}
-            </p>
-          </div>
-          <div>
-            {comment.children?.map((reply) => (
-              <div key={reply.id} className="p-4 my-2 bg-gray-100 rounded-lg">
-                <div className="flex items-center gap-2 mb-2">
-                  <CornerDownRight size={16} className="text-gray-700" />
-                  <CustomAvatar
-                    src={reply.author.profileImageS3SavedURL || ''}
-                    rank={reply.author.rank}
-                    nickname={reply.author.nickname}
-                    avatarSize="xs"
-                  />
-                  <p className="text-sm">{reply.author.nickname}</p>
-                  <div className="ml-auto flex gap-2">
-                    <Button className="bg-transparent p-0">
-                      <MessageSquare
-                        className="text-gray-400 size-4"
-                        onClick={() =>
-                          handleReplyButtonClick(
-                            reply.id.toString(),
-                            reply.author.nickname,
-                          )
-                        }
-                      />
-                    </Button>
-                    <DropdownMenu>
-                      <DropdownMenuTrigger
-                        className={cn(
-                          nickname === reply.author.nickname
-                            ? 'visible'
-                            : 'hidden',
-                          'bg-transparent p-0',
-                        )}
-                      >
-                        <Ellipsis className="text-gray-400 size-4" />
-                        <DropdownMenuContent className="bg-white -mt-2 -ml-8 w-16">
-                          <DropdownMenuItem
-                            className="hover:bg-slate-50 transition-all"
-                            onClick={() =>
-                              handleEditButtonClick(
-                                reply.id.toString(),
-                                reply.content,
-                              )
-                            }
-                          >
-                            수정
-                          </DropdownMenuItem>
-                          <DropdownMenuItem
-                            className="hover:bg-slate-50 transition-all"
-                            onClick={() =>
-                              handleRemoveButtonClick(reply.id.toString())
-                            }
-                          >
-                            삭제
-                          </DropdownMenuItem>
-                        </DropdownMenuContent>
-                      </DropdownMenuTrigger>
-                    </DropdownMenu>
-                  </div>
+    <>
+      {' '}
+      <div className="p-4">
+        <span className="text-gray-700 text-lg font-bold">댓글</span>
+        {commentList.map((comment) => (
+          <div key={comment.id}>
+            <div
+              key={comment.id}
+              className="p-4 my-2 last-of-type:border-none border-b-[1px] border-gray-200"
+            >
+              <div className="flex items-center gap-2 mb-2">
+                <CustomAvatar
+                  src={comment.author.profileImageS3SavedURL || ''}
+                  rank={comment.author.rank}
+                  nickname={comment.author.nickname}
+                  avatarSize="xs"
+                />
+                <p className="text-sm">{comment.author.nickname}</p>
+                <div className="ml-auto flex gap-2">
+                  <Button className="bg-transparent p-0">
+                    <MessageSquare
+                      className="text-gray-400 size-4"
+                      onClick={() =>
+                        handleReplyButtonClick(
+                          comment.id.toString(),
+                          comment.author.nickname,
+                        )
+                      }
+                    />
+                  </Button>
+                  <DropdownMenu>
+                    <DropdownMenuTrigger
+                      className={cn(
+                        nickname === comment.author.nickname
+                          ? 'visible'
+                          : 'hidden',
+                        'ml-auto cursor-pointer',
+                      )}
+                    >
+                      <Ellipsis className="text-gray-400 size-4" />
+                      <DropdownMenuContent className="bg-white -mt-2 -ml-8 w-16">
+                        <DropdownMenuItem
+                          className="hover:bg-slate-50 transition-all"
+                          onClick={() =>
+                            handleEditButtonClick(
+                              comment.id.toString(),
+                              comment.content,
+                            )
+                          }
+                        >
+                          수정
+                        </DropdownMenuItem>
+                        <DropdownMenuItem
+                          className="hover:bg-slate-50 transition-all"
+                          onClick={() => {
+                            setOpenCommentDeleteAlert(true);
+                            setDeleteCommentId(comment.id.toString());
+                          }}
+                        >
+                          삭제
+                        </DropdownMenuItem>
+                      </DropdownMenuContent>
+                    </DropdownMenuTrigger>
+                  </DropdownMenu>
                 </div>
-                <p className="text-sm pl-6">{reply.content}</p>
-                <p className="text-sm pl-6 mt-2 text-gray-500">
-                  {commentTime(reply.createdAt)}
-                </p>
               </div>
-            ))}
+              <p className="text-sm">{comment.content}</p>
+              <p className="text-sm mt-2 text-gray-500">
+                {commentTime(comment.createdAt)}
+              </p>
+            </div>
+            <div>
+              {comment.children?.map((reply) => (
+                <div key={reply.id} className="p-4 my-2 bg-gray-100 rounded-lg">
+                  <div className="flex items-center gap-2 mb-2">
+                    <CornerDownRight size={16} className="text-gray-700" />
+                    <CustomAvatar
+                      src={reply.author.profileImageS3SavedURL || ''}
+                      rank={reply.author.rank}
+                      nickname={reply.author.nickname}
+                      avatarSize="xs"
+                    />
+                    <p className="text-sm">{reply.author.nickname}</p>
+                    <div className="ml-auto flex gap-2">
+                      <Button className="bg-transparent p-0">
+                        <MessageSquare
+                          className="text-gray-400 size-4"
+                          onClick={() =>
+                            handleReplyButtonClick(
+                              reply.id.toString(),
+                              reply.author.nickname,
+                            )
+                          }
+                        />
+                      </Button>
+                      <DropdownMenu>
+                        <DropdownMenuTrigger
+                          className={cn(
+                            nickname === reply.author.nickname
+                              ? 'visible'
+                              : 'hidden',
+                            'bg-transparent p-0',
+                          )}
+                        >
+                          <Ellipsis className="text-gray-400 size-4" />
+                          <DropdownMenuContent className="bg-white -mt-2 -ml-8 w-16">
+                            <DropdownMenuItem
+                              className="hover:bg-slate-50 transition-all"
+                              onClick={() =>
+                                handleEditButtonClick(
+                                  reply.id.toString(),
+                                  reply.content,
+                                )
+                              }
+                            >
+                              수정
+                            </DropdownMenuItem>
+                            <DropdownMenuItem
+                              className="hover:bg-slate-50 transition-all"
+                              onClick={() => {
+                                setOpenReplyDeleteAlert(true);
+                                setDeleteCommentId(comment.id.toString());
+                              }}
+                            >
+                              삭제
+                            </DropdownMenuItem>
+                          </DropdownMenuContent>
+                        </DropdownMenuTrigger>
+                      </DropdownMenu>
+                    </div>
+                  </div>
+                  <p className="text-sm pl-6">{reply.content}</p>
+                  <p className="text-sm pl-6 mt-2 text-gray-500">
+                    {commentTime(reply.createdAt)}
+                  </p>
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
-      ))}
-      <CommentInput
-        articleId={articleId}
-        parentComment={parentComment}
-        setParentComment={setParentComment}
-        editingComment={editingComment}
-        setEditingComment={setEditingComment}
+        ))}
+        <CommentInput
+          articleId={articleId}
+          parentComment={parentComment}
+          setParentComment={setParentComment}
+          editingComment={editingComment}
+          setEditingComment={setEditingComment}
+        />
+      </div>
+      <CustomAlert
+        open={openCommentDeleteAlert}
+        setOpen={setOpenCommentDeleteAlert}
+        title="정말 삭제하시겠습니까?"
+        description="삭제하시면 복구할 수 없습니다."
+        cancelText="아니요"
+        confirmText="네"
+        onConfirm={() => deleteCommentMutation.mutate(deleteCommentId)}
       />
-    </div>
+      <CustomAlert
+        open={openReplyDeleteAlert}
+        setOpen={setOpenReplyDeleteAlert}
+        title="정말 삭제하시겠습니까?"
+        description="삭제하시면 복구할 수 없습니다."
+        cancelText="아니요"
+        confirmText="네"
+        onConfirm={() => deleteCommentMutation.mutate(deleteCommentId)}
+      />
+    </>
   );
 };
 
