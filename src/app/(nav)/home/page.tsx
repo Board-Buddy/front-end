865d73ebef3296c0e03fea0b962963b119954880
yubin <cyoobin@naver.com>
2024-08-09 20:58:15 +0900

    Design: 홈화면 검색창 UI 수정

--- src/app/(nav)/home/page.tsx
+++ src/app/(nav)/home/page.tsx
@@ -6,6 +6,7 @@ import { getRankings } from '@/services/ranking';
 import getQueryClient from '@/utils/getQueryClient';
 import ArticleList from '@/containers/home/ArticleList';
 import NotificationButton from '@/containers/home/NotificationButton';
+import SearchInputMockUp from '@/containers/search/SearchInputMockUp';
 
 export const dynamic = 'force-dynamic';
 export const fetchCache = 'force-no-store';
@@ -22,6 +23,7 @@ const page = async () => {
     <div className="relative">
       <NotificationButton />
       <Banner />
+      <SearchInputMockUp />
       <HydrationBoundary state={dehydrate(queryClient)}>
         <Ranking />
       </HydrationBoundary>
