6d5c0125796d3971b0dfc3c4395b5ffc72c93b48
yubin <cyoobin@naver.com>
2024-08-10 03:08:50 +0900

    Fix: 로그인 재시도 버튼 클릭 시 login-splash 페이지로 이동하도록 수정

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -9,7 +9,7 @@ const error = () => {
       <p className="font-bold mb-2 text-gray-900">
         오류가 발생했습니다. 다시 로그인해주세요.
       </p>
-      <Link href="/login">
+      <Link href="/login-splash">
         <Button className="text-white text-sm font-bold w-28 rounded-3xl h-8">
           로그인하러 가기
         </Button>
