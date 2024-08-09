25926638a7208c5acbd07a516823cfdacc8bb987
yubin <cyoobin@naver.com>
2024-08-09 19:08:45 +0900

    Design: 회원가입 페이지 UI 수정

--- src/app/(auth)/register/additionalSettings/page.tsx
+++ src/app/(auth)/register/additionalSettings/page.tsx
@@ -1,18 +1,28 @@
 import AdditionalSettingForm from '@/containers/register/AdditionalSettingForm';
+import Image from 'next/image';
 
 const page = () => {
   return (
-    <>
-      <div>
-        <span className="font-bold text-xl">추가 정보 입력</span>
-        <p className="text-gray-600 font-semibold">
-          서비스를 이용하시려면 추가 정보 입력이 필요합니다.
-        </p>
+    <div className="w-full h-[calc(100vh-56px)] overflow-y-scroll">
+      <div className="pt-4 pb-8 px-8">
+        <div>
+          <Image
+            src="/images/logo/boardbuddy_logo.png"
+            width={70}
+            height={70}
+            alt="보드버디 로고"
+            className="mb-4"
+          />
+          <span className="font-bold text-xl">추가 정보 입력</span>
+          <p className="text-gray-600 font-semibold">
+            서비스를 이용하시려면 추가 정보 입력이 필요합니다.
+          </p>
+        </div>
+        <div className="mt-6">
+          <AdditionalSettingForm />
+        </div>
       </div>
-      <div className="mt-6">
-        <AdditionalSettingForm />
-      </div>
-    </>
+    </div>
   );
 };
 
--- src/app/(auth)/register/terms/page.tsx
+++ src/app/(auth)/register/terms/page.tsx
@@ -1,16 +1,26 @@
 import TermForm from '@/containers/register/TermForm';
+import Image from 'next/image';
 
 const page = () => {
   return (
-    <>
-      <div>
-        <span className="font-bold text-xl">보드버디 회원가입</span>
-        <p className="text-gray-600 font-semibold">
-          서비스를 이용하려면 약관 동의가 필요합니다.
-        </p>
+    <div className="w-full h-[calc(100vh-56px)]">
+      <div className="pt-4 pb-8 px-8">
+        <div>
+          <Image
+            src="/images/logo/boardbuddy_logo.png"
+            width={70}
+            height={70}
+            alt="보드버디 로고"
+            className="mb-4"
+          />
+          <span className="font-bold text-xl">약관동의</span>
+          <p className="text-gray-600 font-semibold">
+            필수항목 및 선택항목 약관에 동의해 주세요.
+          </p>
+        </div>
+        <TermForm />
       </div>
-      <TermForm />
-    </>
+    </div>
   );
 };
 
