d05c3ac30485d27f0cfb406ee94c9cfe1a6863c2
yubin <cyoobin@naver.com>
2024-08-10 11:18:50 +0900

    Feat: 로딩 스피너를 선택적으로 보여주도록 DeferredComponent 구현 및 추가

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,10 +1,13 @@
 import { LoaderCircleIcon } from 'lucide-react';
+import DeferredComponent from './svg/DeferredComponent';
 
 const Loading = () => {
   return (
-    <div className="flex justify-center items-center h-[200px]">
-      <LoaderCircleIcon className="animate-spin text-primary" />
-    </div>
+    <DeferredComponent>
+      <div className="flex justify-center items-center h-[calc(100vh-53px)]">
+        <LoaderCircleIcon className="animate-spin text-primary" />
+      </div>
+    </DeferredComponent>
   );
 };
 
