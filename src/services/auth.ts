3cc6ead5deb43faabd797e764eaa4da235b7a322
Chae Yubin <cyoobin@naver.com>
2024-11-11 13:11:37 +0900

    Fix: axios->fetch로 수정하여 테스트

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,33 +1,12 @@
+import { API_BASE_URL } from '@/constants/env';
 import api from '@/services';
 
 /** 아이디 중복 검사 API */
 export const checkIdDuplicate = async (id: string) => {
-  try {
-    const response = await api.post(`/auth/username/check`, {
-      username: id,
-    });
-
-    return {
-      status: 'success',
-      data: response.data.data,
-      message: response.data.message,
-    };
-  } catch (error: any) {
-    return {
-      status: error.response.data.status,
-      data: error.response.data.data,
-      message: error.response.data.message,
-    };
-  }
-
   // try {
-  //   const response = await fetch(`${API_BASE_URL}/api/auth/username/check`, {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //     body: JSON.stringify({ username: id }),
-  //   }).then((response) => response.json());
+  //   const response = await api.post(`/auth/username/check`, {
+  //     username: id,
+  //   });
 
   //   return {
   //     status: 'success',
@@ -41,6 +20,28 @@ export const checkIdDuplicate = async (id: string) => {
   //     message: error.response.data.message,
   //   };
   // }
+
+  try {
+    const response = await fetch(`${API_BASE_URL}/auth/username/check`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username: id }),
+    }).then((response) => response.json());
+
+    return {
+      status: 'success',
+      data: response.data.data,
+      message: response.data.message,
+    };
+  } catch (error: any) {
+    return {
+      status: error.response.data.status,
+      data: error.response.data.data,
+      message: error.response.data.message,
+    };
+  }
 };
 
 /** 닉네임 중복 검사 API  */
