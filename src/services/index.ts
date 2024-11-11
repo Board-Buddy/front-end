f87869b4b57905bafefc01c0f67a5d5cd5aaffd0
Chae Yubin <cyoobin@naver.com>
2024-11-11 13:04:37 +0900

    Fix: axios baseURL 수정

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,7 +2,7 @@ import { API_BASE_URL } from '@/constants/env';
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: `${API_BASE_URL}/api`,
   withCredentials: true,
 });
 
