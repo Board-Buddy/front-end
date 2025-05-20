d51dedf8341269bab686fd2022f84e6a28fbcc96
ChaeYubin <cyoobin@naver.com>
2025-05-20 17:48:47 +0900

    Fix: lint 에러 수정
    - index.ts - import 순서 변경
    - endpoint.ts - 문자열 작성 방식을 템플릿 리터럴로 변경

--- src/services/endpoint.ts
+++ src/services/endpoint.ts
@@ -1,5 +1,5 @@
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_SERVER_URL + '/api';
-export const WS_BASE_URL = process.env.NEXT_PUBLIC_WS_SERVER_URL + '/ws';
+export const API_BASE_URL = `${process.env.NEXT_PUBLIC_API_SERVER_URL}/api`;
+export const WS_BASE_URL = `${process.env.NEXT_PUBLIC_WS_SERVER_URL}/ws`;
 
 export const ENDPOINT = {
   AUTH: {
