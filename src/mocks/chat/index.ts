380ea290d6bd2768ac77fb49d21df468eef937cf
ChaeYubin <cyoobin@naver.com>
2025-06-07 14:11:23 +0900

    Feat: 채팅 메시지 조회 모킹 핸들러 로직 변경

--- src/mocks/chat/index.ts
+++ src/mocks/chat/index.ts
@@ -1,6 +1,6 @@
 import { getChatRooms } from './rooms';
 import { getArticleSimpleInfo } from './rooms/[chatRoomId]/gather-articles/[gatherArticleId]';
-import { getExistingMessages } from './rooms/[chatRoomId]/messages';
+import { getExistingMessages } from './messages/[chatRoomId]';
 
 export const chatHandlers = [
   getExistingMessages,
