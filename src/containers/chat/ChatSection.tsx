323e7ab7d5365c2a01530083a39c7e5e865122b1
ChaeYubin <cyoobin@naver.com>
2025-06-07 14:10:48 +0900

    Feat: 채팅 메시지 조회 무한스크롤 지원 기능 추가
    - WebSocketContainer -> ChatContainer로 컴포넌트 명 변경
    - 채팅 메시지 조회용 무한스크롤 옵저버 훅 추가 (기존의 useIntersectionObserver와 조회 방향이 달라 스크롤을 조정하는 로직 추가)

--- src/containers/chat/ChatSection.tsx
+++ src/containers/chat/ChatSection.tsx
@@ -1,20 +1,40 @@
 'use client';
 
 import { Message } from '@/types/chat';
-import { useQueryClient } from '@tanstack/react-query';
+import {
+  InfiniteQueryObserverResult,
+  useQueryClient,
+} from '@tanstack/react-query';
 import { UserInfo } from '@/types/user';
 import { useEffect, useRef } from 'react';
 import ReceivedMessage from './ReceivedMessage';
 import SentMessage from './SentMessage';
 import EnterExitMessage from './EnterExitMessage';
+import { useChatInfiniteScrollObserver } from '@/hooks/custom/useChatInfiniteScrollObserver';
 
-const ChatSection = ({ messages }: { messages: Message[] }) => {
+interface Props {
+  messages: Message[];
+  hasPreviousPage: boolean;
+  fetchPreviousPage: () => Promise<InfiniteQueryObserverResult>;
+}
+
+const ChatSection = ({
+  messages,
+  hasPreviousPage,
+  fetchPreviousPage,
+}: Props) => {
   const cache = useQueryClient();
   const userInfo = cache.getQueryData(['userInfo']) as UserInfo;
   const { nickname } = userInfo;
 
   const scrollRef = useRef<HTMLDivElement>(null);
   const messageEndRef = useRef<HTMLDivElement>(null);
+
+  const { setTarget } = useChatInfiniteScrollObserver({
+    hasPreviousPage,
+    fetchPreviousPage,
+    scrollRef,
+  });
 
   useEffect(() => {
     // 컴포넌트가 처음 마운트될 때 스크롤을 가장 아래로 이동시킨다.
@@ -24,22 +44,33 @@ const ChatSection = ({ messages }: { messages: Message[] }) => {
   }, []);
 
   useEffect(() => {
-    // 새로운 메시지가 추가될 때 스크롤을 조정하여 가장 최근 메시지가 보이도록 한다.
-    messageEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+    const scroll = scrollRef.current;
+    if (!scroll) return;
+
+    const isAtBottom =
+      scroll.scrollHeight - scroll.scrollTop - scroll.clientHeight < 50;
+
+    if (isAtBottom) {
+      // 새로운 메시지가 추가될 때 스크롤을 조정하여 가장 최근 메시지가 보이도록 한다.
+      messageEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+    }
   }, [messages]);
 
   return (
     <div className="h-[calc(100dvh-222px)] overflow-y-auto" ref={scrollRef}>
-      <div className="flex flex-col p-4 gap-4">
-        {messages.map((message, i) => {
-          if (message.messageType === 'TALK') {
-            if (message.nickname === nickname) {
-              return <SentMessage key={i} message={message} />;
+      <div>
+        <div ref={setTarget} className="h-0" />
+        <div className="flex flex-col p-4 gap-4">
+          {messages.map((message, i) => {
+            if (message.messageType === 'TALK') {
+              if (message.nickname === nickname) {
+                return <SentMessage key={i} message={message} />;
+              }
+              return <ReceivedMessage key={i} message={message} />;
             }
-            return <ReceivedMessage key={i} message={message} />;
-          }
-          return <EnterExitMessage key={i} message={message} />;
-        })}
+            return <EnterExitMessage key={i} message={message} />;
+          })}
+        </div>
       </div>
       <div ref={messageEndRef} />
     </div>
---
+++ src/hooks/custom/useChatInfiniteScrollObserver.ts
@@ -0,0 +1,71 @@
+'use client';
+
+import { InfiniteQueryObserverResult } from '@tanstack/react-query';
+import { RefObject, useCallback, useEffect, useRef, useState } from 'react';
+
+interface Props {
+  threshold?: number;
+  hasPreviousPage: boolean | undefined;
+  fetchPreviousPage: () => Promise<InfiniteQueryObserverResult>;
+  scrollRef: RefObject<HTMLDivElement>;
+}
+
+export const useChatInfiniteScrollObserver = ({
+  threshold = 0.1,
+  hasPreviousPage,
+  fetchPreviousPage,
+  scrollRef,
+}: Props) => {
+  const [target, setTarget] = useState<HTMLDivElement | null | undefined>(null);
+  const previousScrollHeightRef = useRef<number | null>(null);
+
+  // fetchPreviousPage 전후로 스크롤 조정
+  const adjustScroll = async () => {
+    const scrollContainer = scrollRef?.current;
+
+    if (scrollContainer) {
+      previousScrollHeightRef.current = scrollContainer.scrollHeight;
+    }
+
+    await fetchPreviousPage();
+
+    if (scrollContainer && previousScrollHeightRef.current !== null) {
+      const previousScrollHeight = previousScrollHeightRef.current;
+
+      // 이중 rAF를 사용해 브라우저가 모든 작업을 완료한 이후임을 보장
+      requestAnimationFrame(() => {
+        requestAnimationFrame(() => {
+          const newScrollHeight = scrollContainer.scrollHeight;
+          const scrollDiff = newScrollHeight - previousScrollHeight;
+          scrollContainer.scrollTop += scrollDiff;
+        });
+      });
+    }
+  };
+
+  const observerCallback = useCallback<IntersectionObserverCallback>(
+    (entries) => {
+      entries.forEach((entry) => {
+        // target이 화면에 관찰되고, 이전 페이지가 있다면 이전 페이지 호출
+        if (entry.isIntersecting && hasPreviousPage) {
+          adjustScroll();
+        }
+      });
+    },
+    [fetchPreviousPage, hasPreviousPage],
+  );
+
+  useEffect(() => {
+    if (!target) return;
+
+    const observer = new IntersectionObserver(observerCallback, {
+      threshold,
+    });
+
+    observer.observe(target);
+
+    return () => observer.unobserve(target);
+  }, [observerCallback, threshold, target]);
+
+  return { setTarget };
+};
