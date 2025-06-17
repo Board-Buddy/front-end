5cc67dcb5e1fa8dea66538cbc6e0ebe5c0bcc491
ChaeYubin <cyoobin@naver.com>
2025-06-17 18:27:12 +0900

    Refactor: 모집글 리스트 조회 데이터 null 대응 로직 추가

--- src/containers/home/ArticleList.tsx
+++ src/containers/home/ArticleList.tsx
@@ -42,34 +42,37 @@ const ArticleList = () => {
       )}
       {!isPending && !isError && (
         <>
-          {data.pages.map((group) => {
-            group.posts && (
-              <div
-                key={group.posts.length > 0 ? group.posts[0].id : 'last-group'}
-                className="flex flex-col gap-y-4"
-              >
-                {group.posts.map((article: IArticle) => (
-                  <Article
-                    onClick={() => router.push(`/article/${article.id}`)}
-                    key={article.id}
-                    id={article.id}
-                    title={article.title}
-                    description={article.description}
-                    author={article.author}
-                    meetingLocation={article.meetingLocation}
-                    maxParticipants={article.maxParticipants}
-                    currentParticipants={article.currentParticipants}
-                    startDateTime={article.startDateTime}
-                    endDateTime={article.endDateTime}
-                    createdAt={article.createdAt}
-                    status={article.status}
-                  />
-                ))}
-              </div>
-            );
-          })}
+          {data.pages.map(
+            (group) =>
+              group.posts && (
+                <div
+                  key={
+                    group.posts.length > 0 ? group.posts[0].id : 'last-group'
+                  }
+                  className="flex flex-col gap-y-4"
+                >
+                  {group.posts.map((article: IArticle) => (
+                    <Article
+                      onClick={() => router.push(`/article/${article.id}`)}
+                      key={article.id}
+                      id={article.id}
+                      title={article.title}
+                      description={article.description}
+                      author={article.author}
+                      meetingLocation={article.meetingLocation}
+                      maxParticipants={article.maxParticipants}
+                      currentParticipants={article.currentParticipants}
+                      startDateTime={article.startDateTime}
+                      endDateTime={article.endDateTime}
+                      createdAt={article.createdAt}
+                      status={article.status}
+                    />
+                  ))}
+                </div>
+              ),
+          )}
           <div className="translate-y-5 text-center text-sm text-gray-600">
-            {data.pages[0].posts.length === 0
+            {data.pages[0].posts === null || data.pages[0].posts.length === 0
               ? '작성된 모집글이 없습니다.'
               : '모든 글을 확인하셨습니다'}
           </div>
