2e9c4e0f96dc81c1d613966568ce896829aea513
ChaeYubin <cyoobin@naver.com>
2025-08-08 21:08:06 +0900

    refactor: 프로필/마이 페이지 로그인 안내 문구 분기 처리

--- src/containers/profile/ProfileContainer.tsx
+++ src/containers/profile/ProfileContainer.tsx
@@ -3,6 +3,7 @@
 import { useUserInfo } from '@/hooks/custom/useUserInfo';
 import { ReactNode } from 'react';
 import LoginPrompt from '@/components/LoginPrompt';
+import { usePathname } from 'next/navigation';
 
 interface Props {
   children: ReactNode;
@@ -10,8 +11,19 @@ interface Props {
 
 const ProfileContainer = ({ children }: Props) => {
   const userInfo = useUserInfo();
+  const pathname = usePathname();
 
-  return <>{userInfo ? children : <LoginPrompt feature="프로필" />}</>;
+  return (
+    <>
+      {userInfo ? (
+        children
+      ) : (
+        <LoginPrompt
+          feature={pathname.includes('my') ? '마이페이지' : '프로필'}
+        />
+      )}
+    </>
+  );
 };
 
 export default ProfileContainer;
