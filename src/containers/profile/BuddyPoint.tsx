cab2bafeae19815f0428e535ef34b45dba4337eb
ChaeYubin <cyoobin@naver.com>
2025-08-15 11:35:39 +0900

    feat: 버디지수 설명 popover 추가
    - 모바일 웹에서 툴팁이 동작하지 않고, 사용 목적에 popover가 더욱 적절하다고 판단해 popover 적용

--- src/containers/profile/BuddyPoint.tsx
+++ src/containers/profile/BuddyPoint.tsx
@@ -1,17 +1,36 @@
 import { Slider } from '@/components/ui/noThumbSlider';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
 import { Profile } from '@/types/profile';
+import { PopoverArrow } from '@radix-ui/react-popover';
 import { AlertCircleIcon } from 'lucide-react';
 
 const BuddyPoint = ({ score }: { score: Profile['buddyScore'] }) => {
   return (
     <div className="mt-4">
       <div className="mb-1 flex items-center">
-        <AlertCircleIcon
-          className="rounded-full bg-gray-600"
-          color="white"
-          size={14}
-        />
-        <p className="ml-1 text-sm font-semibold text-gray-600">버디지수</p>
+        <Popover>
+          <PopoverTrigger asChild>
+            <div className="group flex items-center gap-1 hover:cursor-pointer">
+              <p className="ml-1 text-sm font-semibold text-gray-600 underline group-hover:text-gray-500">
+                버디지수
+              </p>
+              <AlertCircleIcon
+                className="rounded-full bg-gray-600 group-hover:bg-gray-500"
+                color="white"
+                size={18}
+              />
+            </div>
+          </PopoverTrigger>
+          <PopoverContent className="mb-2 bg-white text-sm">
+            <PopoverArrow fill="white" />
+            <p>모임이 끝난 후 다른 플레이어의 후기에 따라 달라져요.</p>
+            <p>좋은 후기를 받으면 점수가 올라가요.</p>
+          </PopoverContent>
+        </Popover>
         <p className="ml-auto text-sm font-semibold text-primary">{score}</p>
       </div>
       <Slider
