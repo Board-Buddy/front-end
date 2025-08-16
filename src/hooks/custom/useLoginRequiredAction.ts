d5dda8bd25a640d07529b4846a4d13803c3a3706
ChaeYubin <cyoobin@naver.com>
2025-08-16 14:55:44 +0900

    chore: userInfo 상태 확인을 위한 임시 로직 추가

--- src/hooks/custom/useLoginRequiredAction.ts
+++ src/hooks/custom/useLoginRequiredAction.ts
@@ -2,12 +2,15 @@
 
 import { useLoginPromptModal } from '@/store/modalStore';
 import { useUserInfo } from './useUserInfo';
+import { sendDebugLogToApp } from '@/utils/webview';
 
 export const useLoginRequiredAction = () => {
   const userInfo = useUserInfo();
   const open = useLoginPromptModal((state) => state.open);
 
   const runIfLoggedIn = (action: () => void) => {
+    sendDebugLogToApp(`runIfLoggedIn userInfo: ${userInfo}`);
+
     if (!userInfo) {
       open();
       return;
