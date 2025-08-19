1fa7dc259142214d7f82c46970ce8b41c07c5adf
ChaeYubin <cyoobin@naver.com>
2025-08-19 21:35:50 +0900

    feat: 인증 여부 확인을 위한 미들웨어 및 미로그인 시 보여줄 안내 페이지 추가

---
+++ src/app/(auth)/login/guide/page.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import useAppRouter from '@/hooks/custom/useAppRouter';
+import Image from 'next/image';
+
+const Page = () => {
+  const router = useAppRouter();
+
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-center gap-8">
+      <Image
+        src="/images/sundy/sundy_dice.png"
+        alt="주사위를 든 썬디"
+        width={120}
+        height={182}
+      />
+      <div className="flex flex-col gap-4">
+        <p className="text-center text-gray-700">
+          세션이 만료되었거나 로그인이 필요한 기능이예요.
+          <br />
+          로그인해 주세요🙂
+        </p>
+        <div className="flex justify-center gap-2">
+          <Button
+            className="w-32 text-white"
+            onClick={() => router.replace({ href: '/login' })}
+          >
+            로그인하러 가기
+          </Button>
+          <Button
+            variant="outline"
+            className="w-32"
+            onClick={() => router.back()}
+          >
+            이전으로 돌아가기
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
