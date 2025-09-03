7fa4f88424545fde35f972c42131b3e6231270f5
ChaeYubin <cyoobin@naver.com>
2025-09-03 13:01:12 +0900

    refactor: resizeFile 함수 타입 명확화, 지원 이미지 타입 제한 및 에러 핸들링 로직 추가

--- src/utils/image.ts
+++ src/utils/image.ts
@@ -1,17 +1,36 @@
 import FileResizer from 'react-image-file-resizer';
 
-export const resizeFile = (file: File) =>
-  new Promise((resolve) => {
-    FileResizer.imageFileResizer(
-      file,
-      1500,
-      1500,
-      'JPEG',
-      80,
-      0,
-      (uri) => {
-        resolve(uri);
-      },
-      'file',
-    );
+const SUPPORTED_IMAGE_TYPES = [
+  'image/jpeg',
+  'image/png',
+  'image/jpg',
+  'image/webp',
+];
+
+export const resizeFile = (file: File): Promise<File> =>
+  new Promise((resolve, reject) => {
+    if (!SUPPORTED_IMAGE_TYPES.includes(file.type)) {
+      throw new Error('지원하지 않는 이미지 포맷입니다.');
+    }
+
+    try {
+      FileResizer.imageFileResizer(
+        file,
+        1500,
+        1500,
+        'WEBP',
+        80,
+        0,
+        (resized) => {
+          if (resized instanceof File) {
+            resolve(resized);
+          } else {
+            reject(new Error('이미지 리사이징에 실패했습니다.'));
+          }
+        },
+        'file',
+      );
+    } catch (error) {
+      reject(error);
+    }
   });
