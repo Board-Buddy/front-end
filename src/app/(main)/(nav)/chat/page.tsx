62fc357b86d9578fd69160e5ea39662da8c57bee
ChaeYubin <cyoobin@naver.com>
2025-11-29 14:24:59 +0900

    chore: 성능 테스트를 위해 prefetch 로직 제거

--- src/app/(main)/(nav)/chat/page.tsx
+++ src/app/(main)/(nav)/chat/page.tsx
@@ -1,24 +1,11 @@
 import ChatRoomList from '@/containers/chat/ChatRoomList';
 import ChatRoomListContainer from '@/containers/chat/ChatRoomListContainer';
-import { getChatRoomList } from '@/services/chat';
-import getQueryClient from '@/utils/getQueryClient';
-import { chatQueryKeys } from '@/utils/queryKeys';
-import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
 
 const page = async () => {
-  const queryClient = getQueryClient();
-
-  await queryClient.prefetchQuery({
-    queryKey: chatQueryKeys.chatRoomList(),
-    queryFn: getChatRoomList,
-  });
-
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <ChatRoomListContainer>
-        <ChatRoomList />
-      </ChatRoomListContainer>
-    </HydrationBoundary>
+    <ChatRoomListContainer>
+      <ChatRoomList />
+    </ChatRoomListContainer>
   );
 };
 
--- src/app/(main)/(nav)/home/page.tsx
+++ src/app/(main)/(nav)/home/page.tsx
@@ -1,29 +1,16 @@
 import Banner from '@/containers/home/Banner';
 import Ranking from '@/containers/home/Ranking';
-import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
-import { getRankings } from '@/services/ranking';
-import getQueryClient from '@/utils/getQueryClient';
 import NotificationButton from '@/containers/home/NotificationButton';
 import ArticleListContainer from '@/containers/home/ArticleListContainer';
-import { rankingQueryKeys } from '@/utils/queryKeys';
 import HomeSearchInput from '@/containers/search/HomeSearchInput';
 
 const page = async () => {
-  const queryClient = getQueryClient();
-
-  await queryClient.prefetchQuery({
-    queryKey: rankingQueryKeys.all,
-    queryFn: getRankings,
-  });
-
   return (
     <div>
       <NotificationButton />
       <Banner />
       <HomeSearchInput />
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <Ranking />
-      </HydrationBoundary>
+      <Ranking />
       <div className="p-8 pt-2">
         <ArticleListContainer />
       </div>
