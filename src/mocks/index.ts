8b823baffca219f528d095bf1f5dafbe23d33296
ChaeYubin <cyoobin@naver.com>
2025-12-01 11:21:55 +0900

    feat: 공용 mock 핸들러 생성 함수 추가

---
+++ src/mocks/index.ts
@@ -0,0 +1,20 @@
+import { ErrorResponse, SuccessResponse } from '@/types/api';
+import { API_BASE_URL } from './../services/endpoint';
+import { DefaultBodyType, http, HttpResponseResolver, PathParams } from 'msw';
+
+interface CreateMockHandlerParams<T> {
+  method: keyof typeof http;
+  endpoint: string;
+  handler: HttpResponseResolver<
+    PathParams,
+    DefaultBodyType,
+    SuccessResponse<T> | ErrorResponse
+  >;
+}
+
+export const createMockHandler = <T>({
+  method,
+  endpoint,
+  handler,
+}: CreateMockHandlerParams<T>) =>
+  http[method](`${API_BASE_URL}/${endpoint}`, handler);
