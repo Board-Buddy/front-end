e731d8c0118fef5d02b15c685055de50ca41a170
ChaeYubin <cyoobin@naver.com>
2025-12-19 17:32:32 +0900

    refactor: 뱃지, 모집글 참가신청, 랭킹 도메인 API 모킹 핸들러 createMockHandler 적용

--- src/mocks/rankings/index.ts
+++ src/mocks/rankings/index.ts
@@ -1,27 +1,34 @@
-import { API_BASE_URL } from '@/services/endpoint';
-import { http, HttpResponse } from 'msw';
+import { delay, HttpResponse } from 'msw';
+import { createMockHandler } from '..';
+import { Ranking } from '@/types/ranking';
 
-export const getRankings = http.get(`${API_BASE_URL}/rankings`, () => {
-  return HttpResponse.json({
-    status: 'success',
-    data: {
-      rankings: [
-        {
-          nickname: '김송송',
-          profileImageSignedURL: null,
-        },
-        {
-          nickname: '김구구',
-          profileImageSignedURL: null,
-        },
-        {
-          nickname: '김나나',
-          profileImageSignedURL: null,
-        },
-      ],
-    },
-    message: '랭킹 조회에 성공했습니다.',
-  });
+export const getRankings = createMockHandler<{ rankings: Ranking[] }>({
+  method: 'get',
+  endpoint: '/rankings',
+  handler: async () => {
+    await delay(300);
+
+    return HttpResponse.json({
+      status: 'success',
+      data: {
+        rankings: [
+          {
+            nickname: '김송송',
+            profileImageSignedURL: null,
+          },
+          {
+            nickname: '김구구',
+            profileImageSignedURL: null,
+          },
+          {
+            nickname: '김나나',
+            profileImageSignedURL: null,
+          },
+        ],
+      },
+      message: '랭킹 조회에 성공했습니다.',
+    });
+  },
 });
 
 export const rankingHandlers = [getRankings];
