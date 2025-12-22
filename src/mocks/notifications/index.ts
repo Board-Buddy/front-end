afacf140d9fcb00daf5e6432d305d5cb3b0dc42f
ChaeYubin <cyoobin@naver.com>
2025-12-22 20:39:44 +0900

    chore: 알림 조회 모킹 API 응답 데이터 수정

--- src/mocks/notifications/index.ts
+++ src/mocks/notifications/index.ts
@@ -21,11 +21,11 @@ export const getNotificationList = createMockHandler<{
             {
               message:
                 "lee 님이 '퇴근 후 보드게임...'의 참가 신청을 취소했습니다.",
-              createdAt: '2024-08-02 14:54',
+              createdAt: '2024-08-02 14:55',
             },
             {
               message: "lee 님이 '퇴근 후 보드게임...'에 참가 신청을 했습니다.",
-              createdAt: '2024-08-02 14:54',
+              createdAt: '2024-08-02 14:56',
             },
           ],
         },
